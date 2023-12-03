b556512a4bab80b1107cefaa38006f48741d9678
BonuKoo <evlogiaseio@gmail.com>
2023-12-03 13:38:57 +0900

    User Service API 구현 섹션 4 Spring Boot API 사용
    HAL Explorer르 이용한 API 테스트
    Spring Security를 이용한 인증 처리
    API 사용을 위한 사용자 인증 처리 구현
    
    "2023-12-01"

--- src/main/java/kr/co/consulting/MyRESTfulService/Controller/UserController.java
+++ src/main/java/kr/co/consulting/MyRESTfulService/Controller/UserController.java
@@ -1,18 +1,32 @@
 package kr.co.consulting.MyRESTfulService.Controller;
 
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import kr.co.consulting.MyRESTfulService.exception.UserNotFoundException;
 import kr.co.consulting.MyRESTfulService.bean.User;
 import kr.co.consulting.MyRESTfulService.dao.UserDaoService;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.List;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @RestController
+@Tag(name = "user-controller", description = "일반 사용자 서비스를 위한 컨트롤러")
 public class UserController {
 
     private UserDaoService service;
@@ -26,21 +40,39 @@ public class UserController {
         return service.findAll();
     }
 
-    /*
-    유저 Class가 검색되지 않았을 경우 - 해당 id의 주소가 없을 경우
-    FoundException을 반환
-     */
+
+    @Operation(summary = "사용자 정보 조회 API", description = "사용자 ID를 이용해서 사용자 상세 정보 조회를 합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK!, 성공"),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST, 입력파라미터 잘못 되었다."),
+            @ApiResponse(responseCode = "404", description = "USER NOT FOUND, 자료 없다"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR , 서버 오류")
+    })
     @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable int id) {
+    public EntityModel<User> retrieveUser(@Parameter(description = "사용자 ID", required = true,example = "1") @PathVariable int id) {
 
         User user = service.findOne(id);
 
         if (user == null){
             throw new UserNotFoundException(String.format("ID[%s] not found", id));
-
         }
 
-        return user;
+        //HATEOAS
+        EntityModel<User> entityModel = EntityModel.of(user);
+        //WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        //this.getClass를 통해 현재 클래스의 Class 객체를 가져오고,
+        //그 후에 methodOn을 통해 매서드에 대한 참조를 생성하는데
+        //이 과정에서 CGlib이 프록시 객체를 생성하는 데 문제가 발생했을 가능성이 있다고 한다.
+
+        // 해결 방법 : 메서드 참조 변경
+        //현재 클래스의 Class 객체를 가져오는 것 대신, 직접 클래스를 지정하여 메소드 참조를 생성한다.
+
+        //WebMvcLinkBuilder linkTo = linkTo(UserController.class, UserController::retrieveAllUsers);
+        WebMvcLinkBuilder linkTo = linkTo(UserController.class).slash("users");
+
+        entityModel.add(linkTo.withRel("all-users"));
+
+        return entityModel;
 
     }
 
